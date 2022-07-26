f9a555667f892643236249e7194466f70a3cba00
Joshua Bray <josh.c.bray@gmail.com>
2022-07-26 11:59:38 +0100

    Add conditional rendering for quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Questionnaire from "./components/Questionnaire";
 import Welcome from "./components/Welcome";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
 
   return (
     <div className="App">
-      <Welcome toggleDisplay={toggleDisplay} />
+      {!display && <Welcome toggleDisplay={toggleDisplay} />}
+      {display && <Questionnaire />}
     </div>
   );
 }
