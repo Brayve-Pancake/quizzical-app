876aa3968a3fe0158be0174ca99a36a96a479bc4
Joshua Bray <josh.c.bray@gmail.com>
2022-07-26 11:06:57 +0100

    Style quiz front page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <div className="App">
-      <p>Hello</p>
       <Welcome toggleDisplay={toggleDisplay} />
     </div>
   );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,8 @@
 export default function Welcome(props) {
   return (
     <div className="welcome">
+      <img src="/src/assets/blobs.svg" className="blobOne" alt="blob" />
+      <img src="/src/assets/blobs2.svg" className="blobTwo" alt="blob2" />
       <h1 className="welcome--title">Quizzical</h1>
       <h4 className="welcome--body">
         If you think you know animals, try hitting 5 on 5
